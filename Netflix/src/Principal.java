2f6df89cdf0b67dff7560cc7a2480e2d41108b73
Yesid Peláez <satur@DESKTOP-JS0I17V>
2021-05-31 16:08:29 -0500

    Manejo de errores con try catch

--- Netflix/src/Principal.java
+++ Netflix/src/Principal.java
@@ -28,47 +28,62 @@ public class Principal {
 	misSeries.get(4).marcarVisto();
 	
 	// Mostrasr películas que ya fueron vistas
-//	try {
-//		
-//	} catch (Exception e) {
-//		
-//	}
-	
-	
-	//Se busca entre el total de película y series 
-	for(int i = 0; i < misPeliculas.size() + misSeries.size(); i++) {
-		// Se evalua primero para las películas
-		if (i < 5) {
-			if(misPeliculas.get(i).esVisto()) {
-				System.out.println(misPeliculas.get(i).getTitulo() + " " + misPeliculas.get(i).tiempoVisto());
+	try {
+		//Se busca entre el total de película y series 
+		for(int i = 0; i < misPeliculas.size() + misSeries.size(); i++) {
+			// Se evalua primero para las películas
+			if (i < 5) {
+				if(misPeliculas.get(i).esVisto()) {
+					System.out.println(misPeliculas.get(i).getTitulo() + " " + misPeliculas.get(i).tiempoVisto());
+				}
+			}
+			//Luego se revisan las series, como i es mayor a 5, se le resta 5 al momento de indexar
+			else {
+				if(misSeries.get(i-5).esVisto()) {
+					System.out.println(misSeries.get(i-5).getTitulo() + " " + misSeries.get(i-5).tiempoVisto());
+				}			
 			}
 		}
-		//Luego se revisan las series, como i es mayor a 5, se le resta 5 al momento de indexar
-		else {
-			if(misSeries.get(i-5).esVisto()) {
-				System.out.println(misSeries.get(i-5).getTitulo() + " " + misSeries.get(i-5).tiempoVisto());
-			}			
-		}
+	} catch (IndexOutOfBoundsException e1) {
+		System.out.println("Estás intendando acceder a un índice fuera del rango: " + "\n" + e1.getMessage());
+	 
+	} catch(Exception e2) {
+		System.out.println(e2.getMessage());
 	}
+	finally {
+		System.out.println();
+	}
+	
+	
+	
+	
+
 	
 	// Buscar serie con más temporadas
 	// inicialmente establezco como el numero max de temporadas con la priemra serie
-	short max_temporada = 0;
-	for(int i = 1; i < misSeries.size(); i++) {
-		if (misSeries.get(i).getNo_temporadas() > misSeries.get(max_temporada).getNo_temporadas()) {
-			max_temporada = (short)i;
-		}
-	}
-	System.out.println("La serie con más temporadas es: "  + misSeries.get(max_temporada).toString()); 
 	
-	//Buscar película más reciente
-	short anio_mas_reciente = 0;
-	for(int i = 1; i < misSeries.size(); i++) {
-		if (misPeliculas.get(i).getAño() > misPeliculas.get(anio_mas_reciente).getAño()) {
-			anio_mas_reciente = (short)i;
+	try {
+		short max_temporada = 0;
+		for(int i = 1; i < misSeries.size(); i++) {
+			if (misSeries.get(i).getNo_temporadas() > misSeries.get(max_temporada).getNo_temporadas()) {
+				max_temporada = (short)i;
+			}
 		}
-	}	
-	System.out.println("La película más reciente es: "  + misPeliculas.get(anio_mas_reciente).toString()); 
+		System.out.println("La serie con más temporadas es: "  + misSeries.get(max_temporada).toString()); 
+		
+		//Buscar película más reciente
+		short anio_mas_reciente = 0;
+		for(int i = 1; i < misSeries.size(); i++) {
+			if (misPeliculas.get(i).getAño() > misPeliculas.get(anio_mas_reciente).getAño()) {
+				anio_mas_reciente = (short)i;
+			}
+		}	
+		System.out.println("La película más reciente es: "  + misPeliculas.get(anio_mas_reciente).toString()); 		
+		
+		
+	} catch (IndexOutOfBoundsException e1) {
+		System.out.println("Estás intendando acceder a un índice fuera del rango: " + "\n" + e1.getMessage());
+	}
 	
 	}
 }
